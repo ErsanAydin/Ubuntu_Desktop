86db3c19d477aa2dc336729a2bc70af496b3552a
ersan <95422447+ErsanAydin@users.noreply.github.com>
2024-09-25 21:00:36 +0300

    Create setup_ubuntudesktop.sh

---
+++ setup_ubuntudesktop.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+echo "Kullanıcı adınızı giriniz (root kullanmayınız) :"
+read kullanici_adi
+
+echo "Parolanızı giriniz: "
+read -s parola
+# Uzak masaüstü için kullanıcı ve şifre değişkenlerini tanımlayın
+# Root kullanmayın
+USER="$kullanici_adi"
+PASSWORD="$parola"
+
+# Paket listesini günceller
+apt update && apt upgrade -y
+
+# GNOME Masaüstünü yükler
+sudo apt install -y ubuntu-desktop
+
+# Uzak masaüstü sunucusunu (xrdp) kurar
+sudo apt install -y xrdp
+
+# USER kullanıcısını şifreyle ekler
+sudo useradd -m -s /bin/bash $USER
+echo "$USER:$PASSWORD" | sudo chpasswd
+
+# USER kullanıcısını yönetim hakları için sudo grubuna ekler
+sudo usermod -aG sudo $USER
+
+# xrdp'yi GNOME masaüstünü kullanacak şekilde yapılandırır
+echo "gnome-session" > ~/.xsession
+
+# xrdp hizmetini yeniden başlatır
+sudo systemctl restart xrdp
+
+# Başlangıçta xrdp'yi etkinleştirir
+sudo systemctl enable xrdp
+
+# Eğer mevcut değilse Belgeler dizinini oluşturur
+sudo -u $USER mkdir -p /home/$USER/Documents
+
+#Chorme Kurulumu
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb 
+
+echo "Kurulum tamamlandi. GNOME Desktop, xrdp ve Chorme kuruldu. Girişi Yapmak için ilk girişte tanımladığınız kullanıcı adı ve parolayı kullanın"
